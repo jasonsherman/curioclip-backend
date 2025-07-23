2f313b5b9abbc1ab696fc5e8a24fd67839142a4a
Ibtisam90 <ibtisam9220@gmail.com>
2025-07-23 18:47:30 +0500

    Update settings to load .env only if it's present

--- curioclip/settings.py
+++ curioclip/settings.py
@@ -17,9 +17,8 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 env = environ.Env()
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+env.read_env(os.path.join(BASE_DIR, '.env')) if os.path.exists(os.path.join(BASE_DIR, '.env')) else None
 
 CELERY_REDIS_HOST=env("CELERY_REDIS_HOST")
 
